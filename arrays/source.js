ae330dae62bddafb31d102f87aa8b8004e7499bb
maannnne <mane.poghosian@gmail.com>
2019-05-13 23:45:42 +0400

    +3 more  tasks on arrays

--- arrays/source.js
+++ arrays/source.js
@@ -1,4 +1,4 @@
-// check if a string is  pangram
+//1.Check if a string is  pangram
     const blankRemover = (arr) => {
         for(let i = 0; i < arr.length; i++){
             if(arr[i] === ' '){
@@ -24,3 +24,51 @@
 
     const sentence = 'The quick brown fox jumps over the lazy dog';
     console.log(isPangram(sentence));
+
+
+
+//2.Write a JavaScript program to remove duplicate items from an array.
+    const dupRemover = (arr) => {
+        let unique = [... new Set(arr)]; //spread operator, Set and new
+        return unique;
+    }
+    let a = [1, 2, 5, 7, 1, 5, 4, 89, 5];
+    console.log(dupRemover(a));
+
+
+
+//3.Write a JavaScript function to sort the following array of objects by title value. 
+    const books = [
+        {
+            title: 'The Hunger Games',
+            author: 'Suzanne Collins'
+        },
+        {
+            title: 'Fahrenheit 451',
+            author: 'Ray Bradury'
+        },
+        {
+            author: 'Paulo Coelho',
+            title: '11 Minutes'
+        },
+        {
+            author: 'Patrick Suskind',
+            title: 'Perfume: The Story of a Murderer'
+        }
+    ];
+    const titles = books.map(book => book.title).sort();
+    console.log(titles);
+
+
+
+//4.Write a JavaScript program to find a pair of elements
+//(indices of the two numbers) from an given array whose sum equals a specific target number
+    const target = 60;
+    const arr = [10, 20, 30, 10, 40, 90, 60];
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i; j < arr.length; j++){
+            if(arr[i] + arr[j] === target){
+                console.log(`${arr[i]} with index of ${i} and ${arr[j]} with index of ${j}`);
+            }
+        }
+    }
