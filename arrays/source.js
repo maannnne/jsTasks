72d27923c50c4632cfdf914972a8d3c23d9c5e4b
maannnne <mane.poghosian@gmail.com>
2020-08-16 16:20:17 +0400

    More on arrrays

--- arrays/source.js
+++ arrays/source.js
@@ -1,94 +1,63 @@
-let arr = [10, 20, 30, 10, 40, 90, 60];
-let arr1 = [1, 2, 3];
+// let arr = [10, 20, 30, 10, 40, 90, 60];
+// let arr1 = [1, 2, 3];
   
-let arr3 = [1,1,1,1,1,1,1];
-let arr4 = [1, 2, 3, 4, 5, 6];
+// let arr3 = [1,1,1,1,1,1,1];
+// let arr4 = [1, 2, 3, 4, 5, 6];
 
 
 
-// // //1.Check if a string is pangram
-// //     const blankRemover = (arr) => {
-// //         for(let i = 0; i < arr.length; i++){
-// //             if(arr[i] === ' '){
-// //                 arr.splice(i, 1);
-// //                 i--;
-// //             }
+// // // //1.Check if a string is pangram
+// // //     const blankRemover = (arr) => {
+// // //         for(let i = 0; i < arr.length; i++){
+// // //             if(arr[i] === ' '){
+// // //                 arr.splice(i, 1);
+// // //                 i--;
+// // //             }
+// // //         }
+// // //         return arr;
+// // //     }
+
+// // //     const isPangram = (sentence) => {
+// // //         const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+// // //         let ok = blankRemover(sentence.toLowerCase().split('').sort());
+// // //         for(let i = 0; i < alphabet.length; i++){
+// // //             if(ok.includes(alphabet[i])){
+// // //                 alphabet.splice(i, 1);
+// // //                 i--;
+// // //             }
+// // //         }
+// // //         return true ? (alphabet.length < 1) : false;    
+
+// // //     }
+
+// // //     const sentence = 'The quick brown fox jumps over the lazy dog';
+// // //     console.log(isPangram(sentence));
+
+
+
+
+
+// // //3.Write a JavaScript function to sort the following array of objects by title value. 
+// //     const books = [
+// //         {
+// //             title: 'The Hunger Games',
+// //             author: 'Suzanne Collins'
+// //         },
+// //         {
+// //             title: 'Fahrenheit 451',
+// //             author: 'Ray Bradury'
+// //         },
+// //         {
+// //             author: 'Paulo Coelho',
+// //             title: '11 Minutes'
+// //         },
+// //         {
+// //             author: 'Patrick Suskind',
+// //             title: 'Perfume: The Story of a Murderer'
 // //         }
-// //         return arr;
-// //     }
-
-// //     const isPangram = (sentence) => {
-// //         const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-// //         let ok = blankRemover(sentence.toLowerCase().split('').sort());
-// //         for(let i = 0; i < alphabet.length; i++){
-// //             if(ok.includes(alphabet[i])){
-// //                 alphabet.splice(i, 1);
-// //                 i--;
-// //             }
-// //         }
-// //         return true ? (alphabet.length < 1) : false;    
-
-// //     }
-
-// //     const sentence = 'The quick brown fox jumps over the lazy dog';
-// //     console.log(isPangram(sentence));
-
-
-
-
-
-// //3.Write a JavaScript function to sort the following array of objects by title value. 
-//     const books = [
-//         {
-//             title: 'The Hunger Games',
-//             author: 'Suzanne Collins'
-//         },
-//         {
-//             title: 'Fahrenheit 451',
-//             author: 'Ray Bradury'
-//         },
-//         {
-//             author: 'Paulo Coelho',
-//             title: '11 Minutes'
-//         },
-//         {
-//             author: 'Patrick Suskind',
-//             title: 'Perfume: The Story of a Murderer'
-//         }
-//     ];
-//     const titles = books.map(book => book.title).sort();
-//     console.log(titles);
-
-
-
-// //4.Write a JavaScript program to find a pair of elements
-// //(indices of the two numbers) from an given array whose sum equals a specific target number
-//     const target = 60;
-    
-//     for(let i = 0; i < arr.length; i++){
-//         for(let j = i; j < arr.length; j++){
-//             if(arr[i] + arr[j] === target){
-//                 console.log(`${arr[i]} with index of ${i} and ${arr[j]} with index of ${j}`);
-//             }
-//         }
-//     }
-
-
-
-// //5.Write a JavaScript function to merge two arrays and remove all duplicates elements.
-//     const merge_and_rem_dup = (arr1, arr2) => {
-//         let merged = arr1.concat(arr2);
-//         const result = merged.filter((elem, index) =>{
-//             return merged.indexOf(elem) === index;
-//         });
-//         return result;   
-//     }
-
-//     console.log(merge_and_rem_dup(arr1, arr2));
-
-
-
-
+// //     ];
+// //     const titles = books.map(book => book.title).sort();
+// //     console.log(titles);
 
 
 //done!!!!!!!!!
@@ -146,3 +115,50 @@ let arr4 = [1, 2, 3, 4, 5, 6];
     }
     let arr6 = ['a', 'b', 'c', 'd', 'e'];
     console.log(remSpecElem(arr6, 2));
+
+
+//Write a JavaScript function to merge two arrays and remove all duplicates elements.
+console.clear();
+const mergeAndRemove = (arr1, arr2) => {
+    let arr = arr1.concat(arr2);
+    const noDupicMupic = arr.filter((elem, index) => {
+        return arr.indexOf(elem) === index;
+    });
+    return noDupicMupic;
+}
+
+let arr7 = ['x', 'y', 'z'];
+let arr8 = ['x', 'w', 'v'];
+console.log(mergeAndRemove(arr7, arr8));
+
+//Write a JavaScript program to find a pair of elements
+//(indices of the two numbers) from an given array whose sum equals a specific target number.
+console.clear();
+const luckyPair = (arr, target) => {
+    let len = arr.length;
+    for(let i = 0; i < len; i++) {
+        for(let j = i; j < len; j++){
+            if(arr[i] + arr[j] === target){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+    return 'No Such pair!';
+}
+
+let arr9 = [1, 4, 8, 3];
+let target = 7;
+console.log(luckyPair(arr9, target));
+
+
+//Write a JavaScript program to shuffle an array.
+const shuffle = (arr) => {
+    let len = arr.length;
+    let randomIndex = Math.floor(Math.random() * len);
+    for(let i = 0; i <= randomIndex; i++) {
+
+    }
+}
+
+let arr10 = [1, 2, 3, 4];
+console.log(shuffle(arr10));
